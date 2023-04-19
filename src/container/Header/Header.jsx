c3da8d3d21bae2584ee9edb0fa971ef0bf318ae9
An Hoang <anvanhoang2014@gmail.com>
2023-04-18 23:09:27 -0400

    fix View More button on home page

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,9 @@ const Header = () => (
       <SubHeading title="Welcome To Flipiya Sushi"/>
       <h1 className="app__header-h1">JAPAN SUSHI BAR</h1>
       <p className="p__opensans" style={{ margin: '2rem 0'}}>Flipiya offers innovative sushi and Japanese cuisine. When you dine with us, you can expect a truly stimulating experience.</p>
-      <button type="button" className="custom__button">Explore Menu</button>
+      <a href="#menu">
+        <button type="button" className="custom__button">Explore Menu</button>
+      </a>
     </div>
     <div className="app__wrapper_img">
       <img src={images.welcome} alt="header img" />
